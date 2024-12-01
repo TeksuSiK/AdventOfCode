f6184fb3e9e0e88590e7efb5e15f46293d8cb831
TeksuSiK <oskar@teksusik.pl>
2024-12-01 12:43:43 +0100

    Prepare for 2024

--- src/main/java/pl/teksusik/aoc/AdventOfCode.java
+++ src/main/java/pl/teksusik/aoc/AdventOfCode.java
@@ -6,9 +6,6 @@ public class AdventOfCode {
     public static void main(String[] args) {
         ChallengeService challengeService = new ChallengeService();
 
-        challengeService.addChallenge(new Day01(1, "01.txt"));
-        challengeService.addChallenge(new Day02(2, "02.txt"));
-
         if (args.length == 0) {
             challengeService.runAllChallenges();
             return;
